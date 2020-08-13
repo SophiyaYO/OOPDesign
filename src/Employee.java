1d28096c1927daf09f244956db27c7c93c67559b
Sofia Yo <dimitrova.sophiya@gmail.com>
2020-08-13 13:27:23 +0300

    added diff classes for different kind of workers

--- src/Employee.java
+++ src/Employee.java
@@ -7,13 +7,31 @@ class MoneyY {
     }
 }
 
-class Worker {
+abstract class Worker {
     private String name;
     public Worker(String name){
         this.name=name;
     }
 
     public  void  pay(){/**/}
+    abstract protected Money getAmountDue();
+}
+
+abstract class Employee extends Worker{
+    private Timesheet[] unpaidTimesheets;
+    public Employee (String name) {
+        super(name);
+    }
+    public void attachTimesheet(Timesheet i) {}
+}
+
+abstract class  Contractor extends Worker{
+    public Contractor(String name) {
+        super(name);
+    }
+    private  Invoice[] invoicesDue;
+    public void attachInvoice(Invoice i) {}
+
 }
 
 class AccountsPayable{
@@ -26,5 +44,4 @@ class AccountsPayable{
     }
 }
 
-public class Employee {
-}
+
