ff8412edd77472355700ae653a9e2a16924bce65
Sofia Yo <dimitrova.sophiya@gmail.com>
2020-08-13 11:33:10 +0300

    naive implementation of simple logic

--- src/Money.java
+++ src/Money.java
@@ -1,3 +1,29 @@
 public class Money {
-    int g = 9;
+    private double value;
+
+    public Money(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+}
+
+class Test{
+    private static void dispenseFunds(Money amount){/**/}
+
+    public static void test() {
+        Money balance = new Money(1.0);
+        Money request = new Money(3.0);
+
+        if (balance.getValue()>request.getValue()){
+            dispenseFunds(request);
+        }
+    }
 }
