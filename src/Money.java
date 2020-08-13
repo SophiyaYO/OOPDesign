6ea0253c52d449c45a520738005daecb3ef37c7f
Sofia Yo <dimitrova.sophiya@gmail.com>
2020-08-13 11:39:52 +0300

    fake methods added to class Test

--- src/Money.java
+++ src/Money.java
@@ -7,9 +7,11 @@ enum Currency{
 
 public class Money {
     private double value;
+    private Currency currency;
 
-    public Money(double value) {
+    public Money(double value, Currency currency) {
         this.value = value;
+        this.currency = currency;
     }
 
     public double getValue() {
@@ -26,11 +28,19 @@ class Test{
     private static void dispenseFunds(Money amount){/**/}
 
     public static void test() {
-        Money balance = new Money(1.0);
-        Money request = new Money(3.0);
+       Money balance = new Money(1.0, Currency.EURO);
+       Money request = new Money(1.0, Currency.USD);
 
-        if (balance.getValue()>request.getValue()){
-            dispenseFunds(request);
-        }
+       double normalizeBalance = balance.getValue() *
+               balance.getCurrency()
+               .conversionRateTo(Currency.USD);
+
+       double normalizeRequest = request.getValue() *
+               request.getCurrency()
+               .conversionRateTo(Currency.USD);
+
+       if (normalizeBalance > normalizeRequest){
+           dispenseFunds(request);
+       }
     }
 }
