8c056634321de2e8d7c8a1a9dfbc9f21f8ad23cd
Sofia Yo <dimitrova.sophiya@gmail.com>
2020-08-14 14:27:15 +0300

    database mock class is created

--- src/testsInOOp/Employee.java
+++ src/testsInOOp/Employee.java
@@ -26,13 +26,31 @@ interface JsonDB {
 
 public class Employee {
     private String name;
-    private static String dbLocation = "http://holub.com/employees";
 
-    public Employee(String id) throws Exception{
-        JsonDB database = new JsonDB.Default();
-        database.open(new URL(dbLocation));
+    public Employee(String id, JsonDB database) throws Exception{
         HashMap<String,String> result = database.lookup(id);
         name = result.get("name");
+    }
+}
+
+class DatabaseMock implements JsonDB{
+    @Override public void open(URL location){}
+    @Override public void close(){}
+
+    @Override
+    public HashMap<String, String> lookup(String key) {
+        HashMap<String,String> employee = new HashMap<>();
+        employee.put("name", "Sofia");
+        return employee;
+    }
+}
+
+class Test{
+    public void test() throws Exception{
+        String dbLocation = "http://holub.com/employees";
+        JsonDB database = new DatabaseMock();
+        database.open(new URL(dbLocation));
+        Employee sofia = new Employee("Sofia", database);
         database.close();
     }
 }
