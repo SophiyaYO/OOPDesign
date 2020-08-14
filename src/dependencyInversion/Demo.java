f9098c16a90f626abc0acb0f694d8a524d0ea8f9
Sofia Yo <dimitrova.sophiya@gmail.com>
2020-08-14 13:26:15 +0300

    added new interfaces and broke the dependancy

--- src/dependencyInversion/Demo.java
+++ src/dependencyInversion/Demo.java
@@ -1,6 +1,10 @@
 package dependencyInversion;
 
-class AccountsPayable{
+interface PaymentObserver{
+    public void amPaying(Payable employee);
+}
+
+class AccountsPayable implements PaymentObserver{
     private  static AccountsPayable instance = new AccountsPayable();
     static AccountsPayable getInstance() {
         return  instance;
@@ -13,13 +17,28 @@ interface Payable{
     public void pay();
 
     public static class Implementation implements Payable{
+        private PaymentObserver observer;
+        public void addObserver(PaymentObserver observer){
+            this.observer = observer;
+        }
+
         public void pay(){
-            AccountsPayable.getInstance().amPaying(this);
+            observer.amPaying(this);
         }
     }
 }
 
+class Peon extends Payable.Implementation{}
+
+
 
 
 public class Demo {
+    public static void main (String[] args){
+        AccountsPayable accounting = new AccountsPayable();
+        Peon worker = new Peon();
+
+        worker.addObserver(accounting);
+        worker.pay();
+    }
 }
